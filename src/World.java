4c3901a0d664501b78b70096f80f02e81ef34883
William Cranney <wcra@Williams-MacBook-Air.local>
2019-02-19 12:38:02 +1100

    passenger rendering

--- src/World.java
+++ src/World.java
@@ -8,6 +8,7 @@ import com.jogamp.opengl.GLProfile;
 import com.jogamp.opengl.awt.GLCanvas;
 import com.jogamp.opengl.awt.GLJPanel;
 import com.jogamp.opengl.glu.GLU;
+import com.jogamp.opengl.util.FPSAnimator;
 
 public class World implements GLEventListener{
 	
@@ -15,6 +16,11 @@ public class World implements GLEventListener{
     public static int N_PLANE_COLS = 7;
     public static int N_PASSENGERS = N_PLANE_ROWS * (N_PLANE_COLS-1);
     public static int BORDER_SIZE = 2;
+    public static int NUM_VERTICES = 16;
+    public static double PASSENGER_RADIUS = 0.25;
+    public static double SEAT_OFFSET = 0.2;
+    
+    private static Passenger[] state;
 	
 	@Override
 	public void display(GLAutoDrawable drawable) {
@@ -28,7 +34,42 @@ public class World implements GLEventListener{
 		gl.glLoadIdentity();
 		
 	    drawPlane(gl);
-	    // draw people
+	    drawState(gl);
+	}
+	
+	// draw the location of each passenger
+	private void drawState(GL2 gl) {
+		for(Passenger p: state) {
+			if(p == null || p.getLoc() == null) continue;
+			drawPassenger(gl, p);
+		}
+	}
+	
+	// draws the passenger as a circle
+	private void drawPassenger(GL2 gl, Passenger p) {
+		
+		double passenger_row = p.getLoc().getRow()+0.5;
+		double passenger_col = p.getLoc().getCol()+0.5;
+		
+		gl.glPolygonMode(GL2.GL_FRONT_AND_BACK, GL2.GL_LINE);
+        gl.glPolygonMode(GL2.GL_FRONT_AND_BACK, GL2.GL_FILL);
+		// set the colour to black
+        gl.glColor3f(0.5f, 0, 0);
+
+		gl.glBegin(GL2.GL_TRIANGLE_FAN);
+	    // set the centre of the circle
+	    gl.glVertex2d(passenger_row, passenger_col); //The centre of the circle
+	    
+	    // draw circle
+		double angle = 0;
+		double angleIncrement = 2 * Math.PI/NUM_VERTICES;
+		for(int i = 0; i <= NUM_VERTICES; i++){
+	        angle = i * angleIncrement;
+	        double x = PASSENGER_RADIUS * Math.cos(angle);
+	        double y = PASSENGER_RADIUS * Math.sin(angle);
+	        gl.glVertex2d(x+passenger_row, y+passenger_col);
+		}
+		gl.glEnd();
 	}
 	
 	// draw the plane seats and aisle
@@ -38,9 +79,9 @@ public class World implements GLEventListener{
 	    // render the seats and aisle for the plane
 	    for(r = 0; r < N_PLANE_ROWS; r++) {
 	    	for(c = 0; c < N_PLANE_COLS; c++) {
-	    		gl.glColor3f(1 ,0f, 0f);
-	    		// don't render the aisle
-	    		if(c == N_PLANE_COLS/2) gl.glColor3f(1, 1, 0f);
+	    		gl.glColor3f(1f, 1f, 1f);
+	    		// render the aisle
+	    		if(c == N_PLANE_COLS/2) gl.glColor3f(0.9f, 0.9f, 0.9f);
 	    		gl.glVertex2d(r, c);
 	    		gl.glVertex2d(r, c+1);
 	    		gl.glVertex2d(r+1, c+1);
@@ -48,7 +89,11 @@ public class World implements GLEventListener{
 	    		
 	    	}
 	    }
-	    gl.glEnd();
+	    gl.glEnd();	    
+	}
+	
+	public void updateState(Passenger[] passengers) {
+		state = passengers;
 	}
 
 	@Override
@@ -62,7 +107,7 @@ public class World implements GLEventListener{
 		GL2 gl = drawable.getGL().getGL2();
 		
 		// set background colour
-		gl.glClearColor(0.5f, 0.8f,  0.98f,  1);
+		gl.glClearColor(0.6f, 0.6f,  0.6f,  1);
 	}
 
 	@Override
@@ -80,11 +125,11 @@ public class World implements GLEventListener{
 	World() {
 		GLProfile profile = GLProfile.get(GLProfile.GL2);
 	    GLCapabilities capabilities = new GLCapabilities(profile);
-	    GLJPanel glcanvas = new GLJPanel(capabilities);
-	    glcanvas.addGLEventListener(this);
+	    GLJPanel canvas = new GLJPanel(capabilities);
+	    canvas.addGLEventListener(this);
 	   
 	    JFrame frame = new JFrame ("Plane Boarding Simulator");
-	    frame.add(glcanvas);
+	    frame.add(canvas);
 	      
 	    // set the default size of the window
 	    frame.setSize(500,500);
@@ -92,6 +137,13 @@ public class World implements GLEventListener{
 	      
 	    // quit on window close
 	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    
+	    //FPSAnimator animator = new FPSAnimator(60);
+	    //animator.add(canvas);
+	    //animator.start();
+	    
+	    // initialise the state, the location of passengers at a given time
+	    state = new Passenger[N_PASSENGERS];
 	}
 
 }
